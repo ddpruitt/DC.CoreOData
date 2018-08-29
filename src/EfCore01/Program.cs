7d320e72a7154ae8b4cca12c0ba89c4620d9a545
Darren Pruitt <ddpruitt@users.noreply.github.com>
2018-08-29 08:52:02 -0500

    Added EF console project

---
+++ src/EfCore01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCore01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
